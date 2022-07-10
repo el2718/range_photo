a996a26ad93d538b31d374de2cb03d9f1ccd2f94
el2718 <45861675+el2718@users.noreply.github.com>
2022-07-10 22:12:13 +0800

    Update range_photo.jl

--- range_photo.jl
+++ range_photo.jl
@@ -1,23 +1,26 @@
 #for julia 1.0
-dir="/photo/"
-dir_out="/photo_ranged/"
+dir_import="/home/e/Desktop/import/"
+dir_ranged="/home/e/Desktop/ranged/"
 
-if !isdir(dir_out)
-	mkdir(dir_out)
+
+print(dir_import)
+if !isdir(dir_ranged)
+	mkdir(dir_ranged)
 end
 
-dir_out_other=string(dir_out,"non-photo/")
-if !isdir(dir_out_other)
-	mkdir(dir_out_other)
+dir_ranged_other=string(dir_ranged,"non-photo/")
+if !isdir(dir_ranged_other)
+	mkdir(dir_ranged_other)
 end
 
-photo=readlines(`find $dir -type f`)
+photo=readlines(`find $dir_import -type f`)
 nphoto=length(photo)
 
-print("\n nphoto=",nphoto,'\n')	
+print("\n nphoto=",nphoto,'\n')
 
-picture_type="png,PNG,jpg,JPG,bmp,BMP"
-media_type="mov,MOV,mp4,MP4,m4a,M4A,3gp,3GP,3g2,3G2,mj2,MJ2,mkv,MKV,mp3,MP3,wav,WAV,avi,AVI,wmv,WMV"
+picture_type="png,PNG,jpg,JPG,jpeg,JPEG,tiff,TIFF,bmp,BMP,raw,RAW,heic,HEIC"
+heic_type="heic,HEIC"
+media_type="mov,MOV,mp4,MP4,m4a,M4A,3gp,3GP,3g2,3G2,mj2,MJ2,mkv,MKV,mp3,MP3,wav,WAV,avi,AVI,wmv,WMV,mts,MTS"
 
 for i=1:nphoto
 	photo_original=photo[i]
@@ -29,45 +32,64 @@ for i=1:nphoto
 	photo_name=photo_name_tmp[end]
 	photo_type=photo_type_tmp[end]
 
-
 	is_picture= occursin(photo_type, picture_type)	
 	is_media= occursin(photo_type, media_type)
 
 	index=nothing
 	
 	if is_picture 
-		info0=readchomp(`identify $photo_original`)
-		info_tmp0=rsplit(info0,"+0+0 ")
-		info_tmp1=rsplit(info_tmp0[1]," ")
-		im_size=rsplit(info_tmp1[end],"x")
-		width=parse(Int, im_size[1])
-		height=parse(Int, im_size[2])
-		if (width < 16e3) & (height < 16e3) & (width*height <256e6)
-			info=readchomp(`identify -verbose $photo_original`)
-			index=findfirst("exif:DateTimeOriginal:",info)		
-		else
-#			info=readchomp(`exiftime $photo_original`)
-#			index=findfirst("Image Digitized:",info)
-#			info=readchomp(`exiftags $photo_original`)
-#			index=findfirst("Date (UTC):",info)
-			info=readchomp(`exif -x $photo_original`)
-			index=findfirst("<Date_and_Time__Original_",info)
+		is_heic= occursin(photo_type, heic_type)
+		if is_heic
+			dir_orig=dirname(photo_original)
+			photo_name_tmp2=rsplit(photo_name,".")
+			photo_jpg=string(dir_orig,"/",photo_name_tmp2[1],".jpg")
+			info=readchomp(Cmd(`heif-convert $photo_original -q 100 $photo_jpg`,ignorestatus=true))
+			
+			if length(info) == 0
+				print("rm ",photo_original,'\n')
+				rm(photo_original)
+				continue
+			end
+			rm(photo_original)
+			photo_name=string(photo_name_tmp2[1],".jpg")
+			photo_original=photo_jpg
 		end
+		
+		if isfile(photo_original)
+			info=readchomp(Cmd(`exiftime $photo_original`,ignorestatus=true))
+			index=findfirst("Image Digitized:",info)
+
+			if index == nothing
+			 	info=readchomp(Cmd(`exiftags $photo_original`,ignorestatus=true))
+				index=findfirst("Date (UTC):",info)
+			end
+			
+
+			if index == nothing
+				info=readchomp(Cmd(`identify -verbose $photo_original`,ignorestatus=true))
+				index=findfirst("exif:DateTimeOriginal:",info)
+			end
+			
+#			if index == nothing
+#				info=readchomp(Cmd(`exif -x $photo_original`,ignorestatus=true))
+#				index=findfirst("<Date_and_Time__Original_",info)
+#			end						
+		end
+
 	end
 	
 	if is_media 
-		info=readchomp(`mediainfo --Output=XML $photo_original`)
+		info=readchomp(Cmd(`mediainfo --Output=XML $photo_original`,ignorestatus=true))
 		index=findfirst("<Encoded_date>UTC",info)
 		if index == nothing
 			index=findfirst("<Encoded_Date>UTC",info)
 		end
 	end
-	#info=readchomp(`stat -c %z $photo_i`)
 	
 	if index != nothing # (isn't a picture & isn't a media) or don't have the infomation of date 
 		year=info[index[end]+2:index[end]+5]
 		month=info[index[end]+7:index[end]+8]
-		dir_year=string(dir_out,year,"/")
+		dir_year=string(dir_ranged,year,"/")
 		dir_month=string(dir_year,month,"/")
 		
 		if !isdir(dir_year)
@@ -75,19 +97,37 @@ for i=1:nphoto
 		end
 	
 		if !isdir(dir_month)
-		  mkdir(dir_month)
+			mkdir(dir_month)
 		end
 		
 		photo_out=string(dir_month,photo_name)
 		
 	else
-
-		photo_out=string(dir_out_other,photo_name)
-
+		photo_out=string(dir_ranged_other,photo_name)
 	end
 
 	print("out:      ",photo_out,'\n')			
 	if !isfile(photo_out)
-		cp(photo_original,photo_out)		
+		mv(photo_original,photo_out)	
+	else
+		if (photo_original!=photo_out)
+			rm(photo_original)
+		end
 	end
-end	
+end
+
+
+photo=readlines(`find $dir_import -type f`)
+nphoto=length(photo)
+if nphoto ==0	
+	dir_import_content=readdir(dir_import)
+	n_content=length(dir_import_content)
+	if n_content!=0
+		curdir=pwd()
+		cd(dir_import)	
+		for i=1:n_content
+			rm(dir_import_content[i], force=true, recursive=true)
+		end
+		cd(curdir)
+	end
+end
